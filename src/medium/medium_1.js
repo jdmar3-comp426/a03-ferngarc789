279020e6fafe90ca0a6ebfa64a2a68e64ec4139d
Fernando Garcia <fernagarc@live.unc.edu>
2021-11-07 17:12:14 -0500

    tseting

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -26,7 +26,7 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-    array.sort()
+    array = array.sort()
     return array
     if (array.length % 2 == 0) {
         return ((array[array.length / 2] + array[(array.length / 2) - 1])) / 2
