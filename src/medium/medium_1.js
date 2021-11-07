547a5d0da8c9fb02dec634e44ced395d6d326212
Fernando Garcia <fernagarc@live.unc.edu>
2021-11-07 17:15:37 -0500

    testing

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -26,7 +26,9 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-    array = array.sort()
+    array.map(String)
+    array.sort()
+    array.map(Integer)
     return array
     if (array.length % 2 == 0) {
         return ((array[array.length / 2] + array[(array.length / 2) - 1])) / 2
