22f9eb352302dcdbe2e0cc85e8a3d25d53fd0703
Fernando Garcia <fernagarc@live.unc.edu>
2021-11-07 16:15:28 -0500

    testing

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -8,7 +8,11 @@ import { variance } from "./data/stats_helpers.js";
  * prototype functions. Very useful
  */
 export function getSum(array) {
-    return sum(array)
+    let i = 0
+    for (let n = 0; n < array.length; n++) {
+        i += array[n];
+    }
+    return i
 }
 
 
