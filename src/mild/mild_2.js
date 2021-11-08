4443dc988dbebe39eb6b8ab2b5bb790525746642
Fernando Garcia <fernagarc@live.unc.edu>
2021-11-07 22:19:15 -0500

    test

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -74,7 +74,9 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+   let newObject = Object.assign(object);
+   delete newObject[key]
+   return newObject
 }
 
 /**
