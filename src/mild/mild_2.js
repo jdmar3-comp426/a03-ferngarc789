f5c5b0b2eed1e64befa7220a2e9342a8faa42f7d
Fernando Garcia <fernagarc@live.unc.edu>
2021-11-07 22:15:54 -0500

    test

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -53,7 +53,8 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-
+   delete object.key;
+   return object;
 }
 
 /**
