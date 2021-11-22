5694f0597a9e9989db893c83bbe640feca6efe4b
Fernando Garcia <ferngarc@live.unc.edu>
2021-11-22 10:58:20 -0500

    test

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -17,7 +17,7 @@
 export const repeat = (fn, n, ...params) => {
     let returnArray = []
     for (let i = 0; i < n; i++) {
-        returnArray[i] = fn(params)
+        returnArray[i] = (fn(params))[0]
     }
     return returnArray
 
